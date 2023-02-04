a23e195088846a0778b5faba55046e665eedb509
tomyamkung <arakawa.kuroda.elec@keio.ac.jp>
2023-02-04 22:32:19 +0900

    auto push

---
+++ tools/auto-commit.py
@@ -0,0 +1,21 @@
+import git
+
+# clone
+url = 'https://github.com/tomyamkung/test-app-a.git'
+to_path = 'checkout-repo'
+git.Repo.clone_from(
+    url,
+    to_path)
+
+# update version
+with open('checkout-repo/test.txt', 'wt') as fp:
+    fp.write('add-string')
+
+# git add    
+repo.git.add('test.txt')
+
+# git commit
+repo.git.commit('test.txt', message='update', author='releaser')
+
+# git push
+repo.git.push ('origin', 'main')
