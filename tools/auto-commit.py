f3fffa49ee436be865a82507ce255305f9ebc37c
tomyamkung <arakawa.kuroda.elec@keio.ac.jp>
2023-02-18 21:58:11 +0900

    addnew branch

--- tools/auto-commit.py
+++ tools/auto-commit.py
@@ -7,16 +7,20 @@ date = datetime.datetime.now()
 # clone
 url = 'git@github.com:tomyamkung/test-app-a.git'
 to_path = 'checkout-repo'
+repo = git.Repo(to_path)
 git.Repo.clone_from(
     url,
     to_path,
     branch='main')
 
+# checkout new branch
+repo.git.branch() 
+repo.git.branch('new_branch') 
+repo.git.checkout('new_branch')
+
 # update version
 with open('checkout-repo/test.txt', 'wt') as fp:
     fp.write(str(date))
-
-repo = git.Repo(to_path)
 
 # git add    
 repo.git.add('test.txt')
@@ -25,4 +29,4 @@ repo.git.add('test.txt')
 repo.git.commit('test.txt','-m','\"auto-commit\"')
 
 # git push
-repo.git.push ('origin', 'main')
+repo.git.push ('origin', 'new_branch')
